156d9e27932f0aee5607ead3e7e4258bb3a26377
Michael Vinicius <michaelvini62@gmail.com>
2022-09-07 21:16:45 -0300

    feat: create store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -5,10 +6,28 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    books: []
   },
   mutations: {
+    setBooks(state, books) {
+      state.books = books
+    }
   },
   actions: {
+    async fetchBooks({ commit }){
+      try {
+        const response = await axios.get('https://ibook-api.herokuapp.com/books');
+        commit('setBooks', response.data);
+      } catch (error) {
+        alert(error)
+        console.error(error)
+      }
+    },
+  },
+  getters: {
+    books(state) {
+      return state.books
+    }
   },
   modules: {
   }
