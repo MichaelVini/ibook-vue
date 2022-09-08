75a1811dec9e7a16208504a5f396e68d09229498
Michael Vinicius <michaelvini62@gmail.com>
2022-09-07 21:14:54 -0300

    feat: create home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Header />
+    <h1>Bem-vindo ao <b>iBook</b></h1>
+    <BookList />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue'; 
+import BookList from '@/components/BookList.vue'; 
 
 @Component({
   components: {
-    HelloWorld,
+    Header,
+    Footer,
+    BookList
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
+<style lang="scss" scoped>
+
+.home {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+}
+
+h1 {
+  font-size: 66px;
+  margin: 80px auto 0 auto;
+  font-weight: 400;
+}
+
+</style>
